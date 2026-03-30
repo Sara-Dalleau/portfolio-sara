f7cd37e22023f9b0cc7de76600d3ec43ea2e3944
Dalleau Sara <sara.dalleau@gmail.com>
2026-03-30 21:50:03 +0200

    Création de la section About: contenu et styles

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,5 +1,26 @@
 function About() {
-  return <section>About</section>
+  return (
+    <section className="about">
+      <div className="about__content">
+        <h2>À propos</h2>
+        <p>
+          Je suis Sara Dalleau, développeuse web junior passionnée par la création d’interfaces modernes et accessibles.
+        </p>
+        <p>
+          Actuellement en formation chez OpenClassrooms, j’ai développé des compétences en HTML, CSS, JavaScript et React, ainsi qu’en back-end avec Node.js et MongoDB.
+        </p>
+        <p>
+          À travers mes projets, j’ai appris à structurer une application, gérer un projet de A à Z et adopter des bonnes pratiques de développement.
+        </p>
+        <p>
+          Je suis particulièrement reconnue pour ma capacité à apprendre rapidement, m’adapter et résoudre des problèmes.
+        </p>
+        <p>
+          Aujourd’hui, je recherche une opportunité pour évoluer au sein d’une équipe et continuer à progresser en tant que développeuse.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default About
