d25fd6a9f544c4e45f16195a2e509635f68837bd
Dalleau Sara <sara.dalleau@gmail.com>
2026-03-30 21:57:07 +0200

    Création de la section Skills: structure et design en cards

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,5 +1,20 @@
 function Skills() {
-  return <section>Skills</section>
+  return (
+    <section className="skills">
+      <h2>Skills</h2>
+
+      <div className="skills__container">
+        <div className="skill-card">HTML</div>
+        <div className="skill-card">CSS</div>
+        <div className="skill-card">JavaScript</div>
+        <div className="skill-card">React</div>
+        <div className="skill-card">Node.js</div>
+        <div className="skill-card">MongoDB</div>
+        <div className="skill-card">Git</div>
+        <div className="skill-card">Méthodologie Agile</div>
+      </div>
+    </section>
+  )
 }
 
 export default Skills
