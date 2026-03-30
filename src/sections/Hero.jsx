4f96b01a2bb1cc257b1c37fc1fd0538500ebcdb4
Dalleau Sara <sara.dalleau@gmail.com>
2026-03-30 21:42:14 +0200

    Création de la section Hero : design et animations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,13 @@
 function Hero() {
-  return <section>Hero</section>
+  return (
+    <section className="hero">
+      <div className="hero__content">
+        <h1>Sara Dalleau</h1>
+        <p>Développeuse web junior</p>
+        <button>Voir mes projets</button>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
