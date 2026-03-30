f1cbbbe0dc700d2724c4b055d60fe44c465f44d0
Dalleau Sara <sara.dalleau@gmail.com>
2026-03-30 22:18:47 +0200

    Création de la section Contact avec liens cliquables et styles

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,5 +1,22 @@
 function Contact() {
-  return <section>Contact</section>
+  return (
+    <section className="contact">
+      <div className="contact__content">
+        <h2>Contact</h2>
+
+        <p>Vous pouvez me contacter via :</p>
+
+        <ul>
+          <li>
+            Email : <a href="mailto:sara.dalleau@gmail.com">sara.dalleau@gmail.com</a>
+          </li>
+          <li>
+            GitHub : <a href="https://github.com/Sara-Dalleau" target="_blank">Sara-Dalleau</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
