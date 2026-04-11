de27c1c8e10265c1fe61bdd0aa518367c06c48b7
Dalleau Sara <sara.dalleau@gmail.com>
2026-04-11 16:45:14 +0200

    feat: ajout backend + routes API

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,51 @@
 const express = require ('express');
 const app = express (); 
 
-app.use ((req, res) => {
-  res.json("Requete ok !");
+app.use(express.json());
+
+//CORS
+app.use((req, res, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
 });
 
+//DATA TEMPORAIRE
+const projects = [
+  {
+    _id: 'abcd321',
+    title: 'Projet 1',
+    description: 'Projet kasa'
+  },
+  {
+    _id: 'abcd123',
+    title: 'Projet 2',
+    description: 'Projet Mon vieux grimoire'
+  },
+];
+
+app.post('/api/projects', (req, res, next) => {
+  projects.push(req.body);
+  res.status(201).json(req.body);
+});
+
+app.get('/api/projects', (req, res, next) => {
+  res.status(200).json(projects);
+});
+
+app.put('/api/projects', (req, res, next) => {
+
+});
+
+app.delete('/api/projects', (req, res, next) => {
+  
+});
 module.exports = app;
